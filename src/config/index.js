32b5fff11c2a4f0605829cb1ec0b0451515960c7
JooYoungHeo <jyhur900226@naver.com>
2019-08-08 22:50:31 +0900

    [Work] config 수정

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,6 @@ import fs from 'fs';
 import path from 'path';
 
 const properties = JSON.parse(fs.readFileSync(path.join(__dirname + '../../../properties.json'), 'utf-8'));
-const config = properties.mysql;
 
-module.exports = config;
+export const mysql = properties.mysql;
+export const redis = properties.redis;
